440ea6a2717d96ffd906d5cf33ede319580211d0
hubin92 <68316494+hubin92@users.noreply.github.com>
2021-08-11 14:40:41 +0800

    Add files via upload

---
+++ trunk/user/dnspod/ddnspod.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+
+# Import ardnspod functions
+. /your_real_path/ardnspod
+
+# Combine your token ID and token together as follows
+arToken="12345,7676f344eaeaea9074c123451234512d"
+
+# Place each domain you want to check as follows
+# you can have multiple arDdnsCheck blocks
+
+# IPv4:
+arDdnsCheck "test.org" "subdomain"
+
+# IPv6:
+arDdnsCheck "test.org" "subdomain6" 6
